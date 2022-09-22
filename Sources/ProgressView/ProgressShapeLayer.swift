6df8a4eb9e304d4f144fab00b49bbbc60536bda7
Julien Guerin <julien.guerin@idean.com>
2022-09-22 16:51:28 +0200

    [ADD] init project

---
+++ Sources/ProgressView/ProgressShapeLayer.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Julien Guerin on 22/09/2022.
+//
+
+import UIKit
+
+class ProgressShapeLayer: CAShapeLayer {
+    
+    public init(strokeColor: UIColor, lineWidth: CGFloat) {
+        super.init()
+        
+        self.strokeColor = strokeColor.cgColor
+        self.lineWidth = lineWidth
+        self.fillColor = UIColor.clear.cgColor
+//        self.lineCap = .round
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
